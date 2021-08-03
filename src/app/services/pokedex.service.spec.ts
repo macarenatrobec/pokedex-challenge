3d8ea7cf09d4d77e0320e7a2925ef554dff300a6
Macarena <Macarena@MacBook-Pro-de-Macarena.local>
2021-08-02 22:43:40 -0300

    jest setup

---
+++ src/app/services/pokedex.service.spec.ts
@@ -0,0 +1,10 @@
+import { TestBed } from '@angular/core/testing';
+import { CardComponent } from '../components/card/card.component';
+
+describe('AppComponent', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [CardComponent],
+    });
+  });
+});
